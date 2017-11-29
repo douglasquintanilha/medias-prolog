cb3af8081808a7ccd37568ad9f5558c38821384b
Douglas Quintanilha Barbosa Ferreira | Stone <dbferreira@stone.com.br>
2017-11-29 16:40:59 -0200

    Sending to github

---
+++ main.pl
@@ -0,0 +1,61 @@
+average(Grades,Weights, Grade) :-
+  average(Grades,Weights,0,0,Grade).
+
+average([],_,SumGrades,SumWeights,Grade) :- Grade is SumGrades / SumWeights.
+average([P|Ps], [W|Ws], SumGrades, SumWeights, Grade) :-
+  NewSumGrades is SumGrades + (P*W),
+  NewSumWeights is SumWeights + W,
+  average(Ps,Ws,NewSumGrades,NewSumWeights,Grade).
+
+get_weights([W1, W2, W3, W4]) :-
+  weight(1,W1),
+  weight(2,W2),
+  weight(3,W3),
+  weight(4,W4).
+
+check_status(row(_,P1,P2,P3,P4,_), Average, "Approved") :- 
+  get_weights(Ws),
+  average([P1,P2,P3,P4], Ws, Average),
+  Average >= 7.0.
+
+check_status(row(_,P1,P2,P3,P4,PF), Grade, "Approved") :- 
+  get_weights(Ws),
+  average([P1,P2,P3,P4], Ws, PN),
+  PN >= 4,
+  Grade is (PN + PF) / 2,
+  Grade >= 5.0.
+
+check_status(row(_,P1,P2,P3,P4,PF), Grade, "Not Approved") :- 
+  get_weights(Ws),
+  average([P1,P2,P3,P4], Ws, PN),
+  PN >= 4,
+  Grade is (PN + PF) / 2.
+
+check_status(row(_,P1,P2,P3,P4,_), Grade, "Not Approved") :- 
+  get_weights(Ws),
+  average([P1,P2,P3,P4], Ws, Grade).
+
+check_approved(X) :-
+  check_status(X,Grade,Status), print_status(X,Grade,Status).
+
+print_status(row(Name,_,_,_,_,_), Grade,Status) :- 
+  nl,
+  print(Name),
+  print(Grade),
+  print(Status).
+
+read_weight(P,W) :-
+  write("Qual é o peso da prova "),
+  write(P),
+  write(" ? "),
+  read(W).
+
+main :- 
+  current_prolog_flag(argv,[File|_]),
+  csv_read_file(File, [_|T]),
+  retractall(weight(_,_)),
+  read_weight(1,W1), assert(weight(1,W1)),
+  read_weight(2,W2), assert(weight(2,W2)),
+  read_weight(3,W3), assert(weight(3,W3)),
+  read_weight(4,W4), assert(weight(4,W4)),
+  maplist(check_approved, T).
